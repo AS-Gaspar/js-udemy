6f55e32b709af864d0980de90429c05c1def81a0
AS-Gaspar <airtontunni@gmail.com>
2024-12-04 15:53:06 -0300

    exercises

--- jsFull/exercicios/numberMath/script.js
+++ jsFull/exercicios/numberMath/script.js
@@ -1,8 +1,19 @@
-let numero = prompt("Digite um número: ");
-numero = Number(numero);
-document.body.innerHTML += `Raiz quadrada: ${Math.sqrt(numero)} <br />`
-document.body.innerHTML += `${numero} é inteiro? ${Number.isInteger(numero)} <br />`;
-document.body.innerHTML += `É NaN: ${Number.isNaN(numero)} <br />`;
-document.body.innerHTML += `Arredondando para baixo: ${Math.floor(numero)} <br />`;
-document.body.innerHTML += `Arredondando par cima: ${Math.ceil(numero)} <br />`;
-document.body.innerHTML += `Com duas casas decimais ${numero.toFixed(2)} <br />`;
+let numero = Number(prompt("Digite um número: "));
+const numeroTitulo = document.getElementsByClassName('numero-titulo');
+const texto = document.getElementById('texto');
+const raiz = document.getElementById('raiz');
+const inteiro = document.getElementById('inteiro');
+const nan = document.getElementById('nan');
+const floor = document.getElementById('floor');
+const ceil = document.getElementById('ceil');
+const fixed = document.getElementById('fixed');
+
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML += `<p>Seu número + 2 é ${numero + 2}</p>`;
+raiz.innerHTML = Math.sqrt(numero);
+inteiro.innerHTML = Number.isInteger(numero);
+nan.innerHTML = Number.isNaN(numero);
+floor.innerHTML = Math.floor(numero);
+ceil.innerHTML = Math.ceil(numero);
+fixed.innerHTML = numero.toFixed(2);
