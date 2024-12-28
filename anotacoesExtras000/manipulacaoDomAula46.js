6c7186a01ea2ed66aa50704cad398b92240653f8
AS-Gaspar <airtontunni@gmail.com>
2024-12-28 16:16:36 -0300

    criação de um cronômetro no navegador

---
+++ anotacoesExtras000/manipulacaoDomAula46.js
@@ -0,0 +1,6 @@
+// innerHTML é a propriedade que retorna o conteúdo HTML de um elemento
+
+// addEventListener server para escutar eventos no documento, no caso, o evento de click
+
+// .target é a propriedade que retorna o elemento que disparou o evento
+
--- logicaDeProgramacao/exercicioCronometro046/assets/js/main.js
+++ logicaDeProgramacao/exercicioCronometro046/assets/js/main.js
@@ -1,18 +1,45 @@
+const getTimeFromSeconds = (miliseconds) => {
+    const data = new Date(miliseconds * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    })
+}
+
+// console.log(getTimeFromSeconds(10))
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let seconds = 0;
+let timer;
 
-iniciar.addEventListener('click', function(event) {
-      
-});
+const startTimer = () => {
+    timer = setInterval(function() {
+        seconds++
+        relogio.innerHTML = getTimeFromSeconds(seconds)
+    }, 1000)
+}
 
-pausar.addEventListener('click', function(event) {
-    
+
+document.addEventListener('click', function(e) {
+    const element = e.target
+    if (element.classList.contains('zerar')) {
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00'
+        seconds = 0
+    }
+
+    if (element.classList.contains('pausar')) {
+        relogio.classList.add('pausado')
+    clearInterval(timer)
+    }
+
+    if (element.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        startTimer()
+    }
 })
-
-zerar.addEventListener('click', function(event) {
-    
-})
-
-const timer = setInterval(
