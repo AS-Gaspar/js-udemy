7179a91567cbd34a0d0e23d194faa9c3347f7d0a
AS-Gaspar <airtontunni@gmail.com>
2025-01-10 07:08:05 -0300

    continuação dos exercicios

--- funcoesAvancado/funcoesGeradoras060/script.js
+++ funcoesAvancado/funcoesGeradoras060/script.js
@@ -8,8 +8,51 @@ function* geradora() {
     yield 'Valor 3'
 }
 
-const g1 = geradora() 
-for (let valor of g1) {
+// const g1 = geradora() 
+// for (let valor of g1) {
+//     console.log(valor)
+// }
+
+// Gereador infinito
+function* geradorInfinito() {
+    let i = 0
+
+    while (true) {
+        yield i
+        i++
+    }
+}
+
+const geradorDeNumeros = geradorInfinito()
+
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+console.log(geradorDeNumeros.next().value)
+
+// Exemplo de gerador que delega tarefas
+function* geradorDeTarefas() {
+    yield 'Tarefa 1'
+    yield 'Tarefa 2'
+    yield 'Tarefa 3'
+}
+
+// 
+function* importaTarefa() {
+    yield* geradorDeTarefas()
+    yield 'Tarefa 4'
+    yield 'Tarefa 5'
+}
+
+const tarefas = importaTarefa()
+for (let valor of tarefas) {
     console.log(valor)
 }
 
