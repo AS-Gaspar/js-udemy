e320f5148379fbd39395385f455cc0c8ec813669
Alexandre <airtontunni@gmail.com>
2025-02-01 01:28:53 -0300

    valida cpf

--- modulos/geraCpf092/src/main.js
+++ modulos/geraCpf092/src/main.js
@@ -1,1 +1,9 @@
+import geraCPF from './modules/geraCPF';
+
 import './assets/css/style.css';
+
+(function() {
+    const gera = new geraCPF
+    const cpfGerado = document.querySelector('.cpf-gerado')
+    cpfGerado.innerHTML = gera.geraNovoCpf()
+})()
